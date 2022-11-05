ec97699a71568fd71100d684e3380592660598d5
Animelmer <duaz.g1@gmail.com>
2022-11-05 10:30:11 -0500

    onsubmit creado

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import * as faceapi from 'face-api.js';
 import { ImagenesModel } from 'src/app/models/imagenes.model';
+import { FormBuilder, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { ImagenesService } from 'src/app/services/imagenes.service';
 
 @Component({
   selector: 'app-upload',
@@ -8,7 +11,6 @@ import { ImagenesModel } from 'src/app/models/imagenes.model';
   styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent implements OnInit {
-
   imgElement = '';
   imgURL = '../../../assets/img/noimage.png';
   imagen: any;
@@ -17,7 +19,16 @@ export class UploadComponent implements OnInit {
   btnActive = true;
   file: any;
 
-  constructor() {}
+  @ViewChild('imageFile', { static: true }) imageFile!: ElementRef;
+
+  imagenesForm = this.fb.group({
+    nombre: ['', [Validators.required]],
+    imgFile: [''],
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  onSubmit() {}
 }
