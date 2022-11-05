5d71a1a7ec147567ebbad32f538be08cd6daa4b0
Animelmer <duaz.g1@gmail.com>
2022-11-05 11:36:53 -0500

    upload imagen msotrar

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -42,9 +42,13 @@ export class UploadComponent implements OnInit {
 
   selectImage(event: any) {
     if (event.target.files.length > 0) {
+
       this.file = event.target.files;
       const reader = new FileReader();
+      reader.readAsDataURL(this.file[0]);
+
       reader.onloadend = (event: any) => {
+
         this.imgURL = event.target.result;
         this.imgElement = event.target.result;
         elementImage.src = `${this.imgElement}`;
@@ -53,37 +57,39 @@ export class UploadComponent implements OnInit {
           archivo: this.file[0],
         };
       };
+      this.btnActive = false;
+
+      var containerImage = document.createElement('div');
+      var status = document.createElement('p');
+      var icon = document.createElement('i');
+      var elementImage = document.createElement('img');
+
+      containerImage.classList.add('containerImage');
+
+      elementImage.crossOrigin = 'anonymous';
+
+      icon.classList.add('fa');
+      icon.classList.add('fa-3x');
+      icon.classList.add('fa-spinner');
+      icon.classList.add('fa-pulse');
+
+      status.classList.add('status');
+
+      status.appendChild(icon);
+
+      containerImage.appendChild(status);
+
+      this.imgProcess = elementImage;
+
+      this.renderer.appendChild(this.imageFile.nativeElement, containerImage);
+
+      this.processFace(this.imgProcess, containerImage);
+
     }
-
-    this.btnActive = false;
-
-    var containerImage = document.createElement('div');
-    var status = document.createElement('p');
-    var icon = document.createElement('i');
-    var elementImage = document.createElement('img');
-
-    containerImage.classList.add('containerImage');
-
-    elementImage.crossOrigin = 'anonymous';
-
-    icon.classList.add('fa');
-    icon.classList.add('fa-3x');
-    icon.classList.add('fa-spinner');
-    icon.classList.add('fa-pulse');
-
-    status.classList.add('status');
-
-    status.appendChild(icon);
-
-    containerImage.appendChild(status);
-
-    this.imgProcess = elementImage;
-
-    this.renderer.appendChild(this.imageFile.nativeElement, containerImage);
   }
 
 
-  
+
   processFace = async (image: any, imageContainer: any) => {
     await faceapi.nets.tinyFaceDetector.loadFromUri('/assets/models');
     await faceapi.nets.faceLandmark68Net.loadFromUri('/assets/models');
@@ -94,27 +100,8 @@ export class UploadComponent implements OnInit {
       .withFaceLandmarks()
       .withFaceDescriptor();
 
-    if (typeof detection === 'undefined') {
-      imageContainer.querySelector('.status').innerText =
-        'No se pudo procesar la imagen';
-      imageContainer.querySelector('.status').style.color = 'red';
+    console.log(detection);
 
-      setTimeout(() => {
-        imageContainer.querySelector('.status').innerText = '';
-        this.imgURL = '../../../assets/img/noimage.png';
-        this.imagenesForm.reset();
-      }, 2000);
-
-      this.btnActive = false;
-    } else {
-      imageContainer.querySelector('.status').innerText = 'Procesado';
-      imageContainer.querySelector('.status').style.color = 'blue';
-      this.onSubmit();
-
-      setTimeout(() => {
-        imageContainer.querySelector('.status').innerText = '';
-      }, 3000);
-    }
   };
 
   onSubmit() {}
